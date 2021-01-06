85310019c2780a82ab0fc027cfe7e5f5dc1c5cd9
famesensor <famfew25@gmail.com>
2021-01-06 22:22:09 +0700

    [update]: import config and set config for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
 })
